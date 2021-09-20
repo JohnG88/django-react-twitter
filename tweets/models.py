c3c6a6688fc2942c80273e30e2280afe32f2b9f2
John Gutierrez <djterez88@gmail.com>
2021-09-19 18:23:26 -0700

    Fixed model class Meta to allow posts to be scene in descending order, added line to clear form, added required attribute to textarea to not be able to send no data

--- tweets/models.py
+++ tweets/models.py
@@ -8,6 +8,11 @@ class Tweet (models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    # reverse ascending order to descending order by id, created, etc...
+    # makemigrations and migrate when doing anything with class
+    class Meta:
+        ordering = ['-created']
 
     def serialize(self):
         return {
