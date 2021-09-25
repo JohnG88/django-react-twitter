cf91dad9cb39a7059dcbfca7fc9eaa3e82a0cb28
John Gutierrez <djterez88@gmail.com>
2021-09-24 20:52:26 -0700

    set useState of likes and userLike, to update like button number

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import {loadTweets} from '../lookup'
+import { loadTweets } from "../lookup";
 
 export function TweetsList(props) {
     const [tweets, setTweets] = useState([{ content: 123 }]);
@@ -27,13 +27,36 @@ export function TweetsList(props) {
 }
 
 export function ActionBtn(props) {
+    const { tweet, action } = props;
+    // remember likes is variable and setLikes is to update variable
+    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0);
+    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false);
     const className = props.className
         ? props.className
         : "btn btn-primary btn-sm";
-    const { tweet, action } = props;
-    return action.type === "like" ? (
-        <button className={className}> {tweet.likes} Like</button>
-    ) : null;
+    // if action.display then show action.display else show 'Action'
+    const actionDisplay = action.display ? action.display : "Action";
+    // if action type = 'like' show tweet likes actionDisplay, else show actionDisplay
+    const display =
+        action.type === "like" ? `${likes} ${actionDisplay}` : actionDisplay;
+    const handleClick = (event) => {
+        event.preventDefault();
+        if (action.type === "like") {
+            if (userLike === true) {
+                // perhaps unlike it?
+                setLikes(likes - 1);
+                setUserLike(false);
+            } else {
+                setLikes(likes + 1);
+                setUserLike(true);
+            }
+        }
+    };
+    return (
+        <button className={className} onClick={handleClick}>
+            {display}
+        </button>
+    );
 }
 
 export function Tweet(props) {
@@ -48,8 +71,18 @@ export function Tweet(props) {
                     {tweet.id} - {tweet.content}
                 </p>
                 <div className="btn btn-group">
-                    <ActionBtn tweet={tweet} action={{ type: "like" }} />
-                    <ActionBtn tweet={tweet} action={{ type: "unlike" }} />
+                    <ActionBtn
+                        tweet={tweet}
+                        action={{ type: "like", display: "Likes" }}
+                    />
+                    <ActionBtn
+                        tweet={tweet}
+                        action={{ type: "unlike", display: "Unlike" }}
+                    />
+                    <ActionBtn
+                        tweet={tweet}
+                        action={{ type: "retweet", display: "Retweet" }}
+                    />
                 </div>
             </div>
         </>
