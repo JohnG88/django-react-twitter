670d8567d7372f0cb76246227dd97d7912540ce2
John Gutierrez <djterez88@gmail.com>
2021-09-26 14:31:31 -0700

    Updated twist_list_view to accept username of url

--- tweets/views.py
+++ tweets/views.py
@@ -106,6 +106,10 @@ def tweet_action_view(request, *args, **kwargs):
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
+    # belo is lookin for parameter ?username=john
+    username = request.GET.get('username')
+    if username != None:
+        qs = qs.filter(user__username__iexact=username)
     serializer = TweetSerializer(qs, many=True)
     return Response(serializer.data, status=200)
     
