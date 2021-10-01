08194507ab0e0484d50c5863fc5fa7fd8a98dd36
John Gutierrez <djterez88@gmail.com>
2021-09-30 21:19:29 -0700

    Created followers field and FollowersRelation model to allow follow functionality and als created some followers test in profiles tests.py

--- profiles/models.py
+++ profiles/models.py
@@ -7,10 +7,23 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
+    '''
+        project_obj = Profile.objects.first()
+        project_obj.followers.all() -> All users following this profile
+        user.following.all() -> all users I follow
+    '''
 
 # method is user_is_saved(so you have to use the function name)
 # post_save signals are sender instance created
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Profile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='cfe', password='somepassword')
+        self.userb = User.objects.create_user(username='cfe-2', password='somepassword2')
+    
+    # checking to see how many profiles are created
+    def test_profile_created_via_signal(self):
+        qs = Profile.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_following(self):
+        first = self.user
+        second = self.userb
+        # add second profile into followers field
+        first.profile.followers.add(second)
+        second_user_following_whom = second.following.all()
+        # checking to see if user is being followed
+        qs = second.following.filter(user=first)
+        self.assertTrue(qs.exists())
+        # first user should follow no one
+        first_user_following_no_one = first.following.all()
+        self.assertFalse(first_user_following_no_one.exists())
